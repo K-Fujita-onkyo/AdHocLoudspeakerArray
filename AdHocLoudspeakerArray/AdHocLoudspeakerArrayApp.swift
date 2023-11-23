dc42b279c50313f52857caad31833786b816ad83
K-Fujita-onkyo <m5261108.aizu@gmail.com>
2023-11-23 18:39:20 +0900

    Initial Commit

---
+++ AdHocLoudspeakerArray/AdHocLoudspeakerArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdHocLoudspeakerArrayApp.swift
+//  AdHocLoudspeakerArray
+//
+//  Created by 藤田一旗 on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AdHocLoudspeakerArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
